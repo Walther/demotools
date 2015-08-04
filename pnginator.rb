344b28f5bab6e827ea61d98e3ec09a047db8ad02
Walther <veeti.haapsamo@gmail.com>
2015-08-04 14:01:28 +0300

    simplify pnginator

--- pnginator.rb
+++ pnginator.rb
@@ -6,8 +6,10 @@
 
 # Usage: ruby pnginator.rb input.js output.png.html
 
-# By Gasman <http://matt.west.co.tt/> https://gist.github.com/gasman/2560551
-# from an original idea by Daeken: http://daeken.com/superpacking-js-demos
+# This version edited by Walther
+# Original work by Gasman <http://matt.west.co.tt/> https://gist.github.com/gasman/2560551
+# based on an original idea by Daeken: http://daeken.com/superpacking-js-demos
+
 
 
 MAX_WIDTH = 4096
@@ -22,31 +24,17 @@ f = File.open(input_filename, 'rb')
 js = f.read
 f.close
 
-if js.length < MAX_WIDTH
-    # js fits onto one pixel line
-    js += "\x00"
-    scanlines = [js]
-    width = js.length
-    height = 1
+js = "\x00" + js
+width = MAX_WIDTH
+# split js into scanlines of 'width' pixels; pad the last one with whitespace
+scanlines = js.scan(/.{1,#{width}}/m).collect{|line| line.ljust(width, "\x00")}
+height = scanlines.length
 
-    # Daeken's single-pixel-row bootstrap (requires js string to be reversed)
-    # (edit by Gasman: change eval to (1,eval) to force global evaluation and avoid massive slowdown)
-    # html = "<canvas id=q><img onload=for(p=q.width=#{width},(c=q.getContext('2d')).drawImage(this,0,e='');p;)e+=String.fromCharCode(c.getImageData(--p,0,1,1).data[0]);(1,eval)(e) src=#>"
+# p01's multiple-pixel-row bootstrap (requires a dummy first byte on the js string)
+# (edit by Gasman: set explicit canvas width to support widths above 300; move drawImage out of getImageData params; change eval to (1,eval) to force global evaluation)
+# (edit by Walther: use full viewport of the browser for the canvas, "fullscreen"
+html = "<head><style type='text/css'>body{margin:0;padding:0;float:left;display:block;position:absolute;top:0;width:100%;height:100%;overflow:hidden;}canvas{margin:0;padding:0;float:left;display:block;position:absolute;top:0;}</style></head><body><canvas id='c' style='display:none;'><img onload=for(w=c.width=#{width},a=c.getContext('2d'),a.drawImage(this,p=0,0),e='',d=a.getImageData(0,0,w,#{height}).data;t=d[p+=4];)e+=String.fromCharCode(t);(1,eval)(e) src=#>"
 
-    # p01's single-pixel-row bootstrap (requires an 0x00 end marker on the js string)
-    # (edit by Gasman: move drawImage out of getImageData params (it returns undef, which is invalid) and change eval to (1,eval) to force global evaluation)
-    html = "<canvas id=c><img onload=with(c.getContext('2d'))for(p=e='';drawImage(this,p--,0),t=getImageData(0,0,1,1).data[0];)e+=String.fromCharCode(t);(1,eval)(e) src=#>"
-else
-    js = "\x00" + js
-    width = MAX_WIDTH
-    # split js into scanlines of 'width' pixels; pad the last one with whitespace
-    scanlines = js.scan(/.{1,#{width}}/m).collect{|line| line.ljust(width, "\x00")}
-    height = scanlines.length
-
-    # p01's multiple-pixel-row bootstrap (requires a dummy first byte on the js string)
-    # (edit by Gasman: set explicit canvas width to support widths above 300; move drawImage out of getImageData params; change eval to (1,eval) to force global evaluation)
-    html = "<canvas id=c><img onload=for(w=c.width=#{width},a=c.getContext('2d'),a.drawImage(this,p=0,0),e='',d=a.getImageData(0,0,w,#{height}).data;t=d[p+=4];)e+=String.fromCharCode(t);(1,eval)(e) src=#>"
-end
 
 # prepend each scanline with 0x00 to indicate 'no filtering', then concat into one string
 image_data = scanlines.collect{|line| "\x00" + line}.join
@@ -56,37 +44,37 @@ def png_chunk(signature, data)
     [data.length, signature, data, Zlib::crc32(signature + data)].pack("NA4A*N")
 end
 
-if USE_PNGOUT
-    # Create a valid (no format hacks) .png file to pass to pngout
-    f = Tempfile.open(['pnginator', '.png'])
 
-    begin
-        f.write("\x89PNG\x0d\x0a\x1a\x0a") # PNG file header
-        f.write(png_chunk("IHDR", [width, height, 8, 0, 0, 0, 0].pack("NNccccc")))
-        f.write(png_chunk("IDAT", idat_chunk))
-        f.write(png_chunk("IEND", ''))
-        f.close
+# Create a valid (no format hacks) .png file to pass to pngout
+f = Tempfile.open(['pnginator', '.png'])
 
-        system("pngout", f.path, "-c0", "-y")
+begin
+    f.write("\x89PNG\x0d\x0a\x1a\x0a") # PNG file header
+    f.write(png_chunk("IHDR", [width, height, 8, 0, 0, 0, 0].pack("NNccccc")))
+    f.write(png_chunk("IDAT", idat_chunk))
+    f.write(png_chunk("IEND", ''))
+    f.close
 
-        # read file back and extract the IDAT chunk
-        f.open
-        f.read(8)
-        while !f.eof?
-            length, signature = f.read(8).unpack("NA4")
-            data = f.read(length)
-            crc = f.read(4)
+    system("pngout", f.path, "-c0", "-y")
 
-            if signature == "IDAT"
-                idat_chunk = data
-                break
-            end
+    # read file back and extract the IDAT chunk
+    f.open
+    f.read(8)
+    while !f.eof?
+        length, signature = f.read(8).unpack("NA4")
+        data = f.read(length)
+        crc = f.read(4)
+
+        if signature == "IDAT"
+            idat_chunk = data
+            break
         end
-    ensure
-        f.close
-        f.unlink
     end
+ensure
+    f.close
+    f.unlink
 end
+
 
 File.open(output_filename, 'wb') do |f|
     f.write("\x89PNG\x0d\x0a\x1a\x0a") # PNG file header
